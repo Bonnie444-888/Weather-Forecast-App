59c69c2bd1cf73dd2793b2d5d257c139f39f657b
Bonnie444-888 <isabel.kedibone@gmail.com>
2024-08-15 13:52:00 +0200

    Implemented search functionallity using API

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
+function updateWeatherInfo(response) {
+  let cityElement = document.querySelector("#city");
+  let temperatureElement = document.querySelector("#temperature-value");
+  let currentTemperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(currentTemperature);
+}
+
+function searchCity(city) {
+  let apiKey = "c7bafob10t4b2f7e01b7bfdba1fa34ec";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeatherInfo);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Middelburg");
